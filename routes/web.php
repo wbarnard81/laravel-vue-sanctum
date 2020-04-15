df2b9011f36d5798152b964b2f9b92102efd5fe1
Werner Barnard <wbarnard81@gmail.com>
2020-04-15 15:15:36 +0200

    Minor changes made to the view

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('{path}', function () {
-    return view('welcome');
-})->where('path', '(.*)');
 
 Auth::routes();
+
+Route::get('{any}', 'AppController@index')->where('any', '.*');
